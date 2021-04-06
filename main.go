aee9c705706c434dd599bcec75ae5f5c84170ad3
Bisakh Mondal <bisakhmondal00@gmail.com>
2021-04-07 02:41:51 +0530

    feat: main restructured

--- main.go
+++ main.go
@@ -22,30 +22,41 @@ import (
 
 var (
 	timeout      = flag.Int("timeout", 20, "context timeout in seconds")
-	repoURL      = flag.String("repo", "https://chromium.googlesource.com/chromiumos/platform/tast", "Repository URL")
+	repoURL      = flag.String("repo", "/chromium.googlesource.com/chromiumos/platform/tast", "Repository URL")
 	repoBranch   = flag.String("branch", "main", "branch name where the parser should run")
 	saveLocation = flag.String("dir", "./commits", "folder where parsed commit messages is going to be stored")
 	csvPath      = flag.String("csvpath", "stats.csv", "csv file location where the details statistics is going to be stored")
 	numCommits   = flag.Int("commits", 20, "Number of commits to be parsed")
 )
 
-var (
-	pool *container.Container
-	wb   *utils.WriteBuffer
-)
-
 func main() {
 	flag.Parse()
 	PrintInfo()
 
-	pool = container.NewContainer()
-	wb = utils.NewWriteBuffer()
+	if err := scraper(); err != nil {
+		panic(err)
+	}
 
-	//incase context timed-out
-	//persist already fetched data
-	defer func() {
-		must(wb.DumpContent(*saveLocation))
-		must(pool.WriteCSV(*csvPath))
+	fmt.Println("Execution Complete")
+}
+
+func scraper() error {
+
+	// A container which keeps track of
+	pool := container.NewContainer()
+	// A temporary buffer to store commit messages with commitID as filename, which is going to be flushed into files later
+	wb := utils.NewWriteBuffer()
+
+	var err error
+
+	// In case context timed-out, persist already fetched data
+	defer func() error {
+		err = wb.DumpContent(*saveLocation)
+		if err != nil {
+			return err
+		}
+		err = pool.WriteCSV(*csvPath)
+		return err
 	}()
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(*timeout)*time.Second)
@@ -58,6 +69,7 @@ func main() {
 
 	go func() {
 		<-sig
+		fmt.Println("Exiting...")
 		cancel()
 	}()
 
@@ -65,111 +77,166 @@ func main() {
 	pt, err := devt.Get(ctx, devtool.Page)
 	if err != nil {
 		pt, err = devt.Create(ctx)
-		must(err)
+		return err
 	}
 
 	conn, err := rpcc.DialContext(ctx, pt.WebSocketDebuggerURL)
-	must(err)
+	if err != nil {
+		return err
+	}
 	defer conn.Close()
 
-	c := cdp.NewClient(conn)
+	client := cdp.NewClient(conn)
 
-	domContent, err := c.Page.DOMContentEventFired(ctx)
-	must(err)
+	domContent, err := client.Page.DOMContentEventFired(ctx)
+	if err != nil {
+		return err
+	}
 	defer domContent.Close()
 
-	err = c.Page.Enable(ctx)
-	must(err)
-
-	FULLURL := strings.Trim(*repoURL, "/")
-	if !strings.HasPrefix(FULLURL, "https://") {
-		FULLURL = "https://" + FULLURL
+	err = client.Page.Enable(ctx)
+	if err != nil {
+		return err
 	}
 
-	splits := strings.Split(FULLURL, "/")
-	HARDURL := strings.Join(splits[:3], "/")
-	SOFTURL := "/" + strings.Join(splits[3:], "/")
-
-	defaultBranch := *repoBranch
-	//switch to a branch & will be checked in UnwrapPre if it exists
-	SOFTURL += "/+/refs/heads/" + defaultBranch
+	hardURL, softURL := formatURL()
 
 	for cnt := 0; cnt < *numCommits; cnt++ {
+
 		fmt.Println("Commit No: ", cnt+1)
-		navArgs := page.NewNavigateArgs(HARDURL + SOFTURL)
-		_, err := c.Page.Navigate(ctx, navArgs)
-		must(err)
+		// Navigate to next commit
+		navArgs := page.NewNavigateArgs(hardURL + softURL)
+		_, err = client.Page.Navigate(ctx, navArgs)
+		if err != nil {
+			return err
+		}
 
 		//wait until dom loaded successfully
 		_, err = domContent.Recv()
-		must(err)
-		//fmt.Printf("Page loaded with frame ID: %s\n", nav.FrameID)
+		if err != nil {
+			return err
+		}
 
-		doc, err := c.DOM.GetDocument(ctx, nil)
-		must(err)
+		nextCommitAnchor, err := parseCommitPage(ctx, client, pool, wb)
+		if err != nil {
+			return err
+		}
 
-		//Fetch Commit Message
-		pre, err := c.DOM.QuerySelector(ctx, dom.NewQuerySelectorArgs(doc.Root.NodeID, "pre"))
+		// Load next commit's relative url
+		softURL = nextCommitAnchor.Url
 
-		preRes, err := c.DOM.GetOuterHTML(ctx, &dom.GetOuterHTMLArgs{
-			NodeID: &pre.NodeID,
-		})
+		// bypass rate limiting quota for multiple request, please give larger timeout
+		if (cnt+1)%50 == 0 {
+			time.Sleep(5 * time.Second)
+			if cnt >= 150 {
+				time.Sleep(5 * time.Second)
+			}
+		}
+	}
+	return err
+}
 
-		parsedPre, err := utils.UnwrapPre(preRes.OuterHTML)
-		must(err)
-
-		revBy := utils.ExtractIdentity(parsedPre.RevBy)
-		pool.AddReview(revBy)
-
-		//fmt.Println(parsedPre.Message)
-
-		//getCurrentCommitID & Commit Author
-		tableDatas, err := c.DOM.QuerySelectorAll(ctx, &dom.QuerySelectorAllArgs{
-			NodeID:   doc.Root.NodeID,
-			Selector: "td",
-		})
-
-		commitHTML, err := c.DOM.GetOuterHTML(ctx, &dom.GetOuterHTMLArgs{
-			NodeID: &tableDatas.NodeIDs[0],
-		})
-
-		commitID := utils.UnwrapTd(commitHTML.OuterHTML)
-
-		//fmt.Println(commitID)
-
-		//add commitId as filename and body as message to writebuffer
-		wb.AppendContent(parsedPre.Message, commitID)
-
-		//commitAuthor
-		authorHTML, err := c.DOM.GetOuterHTML(ctx, &dom.GetOuterHTMLArgs{
-			NodeID: &tableDatas.NodeIDs[2],
-		})
-
-		authorRaw := utils.UnwrapTd(authorHTML.OuterHTML)
-
-		author := utils.ExtractIdentity(authorRaw)
-
-		pool.AddCommit(author)
-		//fmt.Printf("%+v", author)
-
-		//get next url
-		hyperlinks, err := c.DOM.QuerySelector(ctx, &dom.QuerySelectorArgs{
-			NodeID:   tableDatas.NodeIDs[len(tableDatas.NodeIDs)-1],
-			Selector: "a",
-		})
-
-		anchorTag, err := c.DOM.GetOuterHTML(ctx, &dom.GetOuterHTMLArgs{
-			NodeID: &hyperlinks.NodeID,
-		})
-
-		nextCommitAnchor := utils.UnwrapA(anchorTag.OuterHTML)
-		//fmt.Printf("%+v", nextCommitAnchor)
-
-		SOFTURL = nextCommitAnchor.Url
-		//fmt.Println("-----------------------------------------------------\n-------------------------------------------------------")
-
+func parseCommitPage(ctx context.Context, client *cdp.Client, pool *container.Container, wb *utils.WriteBuffer) (*utils.Anchor, error) {
+	// Get the whole DOM
+	doc, err := client.DOM.GetDocument(ctx, nil)
+	if err != nil {
+		return nil, err
 	}
 
+	// Fetch Commit Message
+	pre, err := client.DOM.QuerySelector(ctx, dom.NewQuerySelectorArgs(doc.Root.NodeID, "pre"))
+	if err != nil {
+		return nil, &utils.DOMError{Err: err}
+	}
+	preRes, err := client.DOM.GetOuterHTML(ctx, &dom.GetOuterHTMLArgs{
+		NodeID: &pre.NodeID,
+	})
+	if err != nil {
+		return nil, &utils.DOMError{Err: err}
+	}
+
+	// Extract Reviewed-By and Commit message
+	parsedPre, err := utils.UnwrapPre(preRes.OuterHTML)
+	if err != nil {
+		return nil, err
+	}
+
+	// Parsed email, name of Reviewed-BY
+	revBy := utils.ExtractIdentity(parsedPre.RevBy)
+	// Increment his/her review count
+	pool.AddReview(revBy)
+
+	// Current commitID & commit author & next url
+	tableDatas, err := client.DOM.QuerySelectorAll(ctx, &dom.QuerySelectorAllArgs{
+		NodeID:   doc.Root.NodeID,
+		Selector: "td",
+	})
+	if err != nil {
+		return nil, &utils.DOMError{Err: err}
+	}
+	commitHTML, err := client.DOM.GetOuterHTML(ctx, &dom.GetOuterHTMLArgs{
+		NodeID: &tableDatas.NodeIDs[0],
+	})
+	if err != nil {
+		return nil, &utils.DOMError{Err: err}
+	}
+
+	// Parsed commit ID as unique filename
+	commitID := utils.UnwrapTd(commitHTML.OuterHTML)
+
+	// add commitId as filename and body as message to the temporary buffer
+	wb.AppendContent(parsedPre.Message, commitID)
+
+	// Get commit author
+	authorHTML, err := client.DOM.GetOuterHTML(ctx, &dom.GetOuterHTMLArgs{
+		NodeID: &tableDatas.NodeIDs[2],
+	})
+	if err != nil {
+		return nil, &utils.DOMError{Err: err}
+	}
+
+	// Unwrap author from HTML tag
+	authorRaw := utils.UnwrapTd(authorHTML.OuterHTML)
+	// Parse email and name from string
+	author := utils.ExtractIdentity(authorRaw)
+
+	// Increment his/her commit count
+	pool.AddCommit(author)
+
+	// Get next commit url
+	hyperlinks, err := client.DOM.QuerySelector(ctx, &dom.QuerySelectorArgs{
+		NodeID:   tableDatas.NodeIDs[len(tableDatas.NodeIDs)-1],
+		Selector: "a",
+	})
+	if err != nil {
+		return nil, &utils.DOMError{Err: err}
+	}
+	anchorTag, err := client.DOM.GetOuterHTML(ctx, &dom.GetOuterHTMLArgs{
+		NodeID: &hyperlinks.NodeID,
+	})
+	if err != nil {
+		return nil, &utils.DOMError{Err: err}
+	}
+
+	// Parse url from href field in anchor tag
+	nextCommitAnchor := utils.UnwrapA(anchorTag.OuterHTML)
+	return nextCommitAnchor, nil
+}
+
+func formatURL() (hardURL string, softURL string) {
+	trimmedURL := strings.Trim(*repoURL, "/")
+	if !strings.HasPrefix(trimmedURL, "https://") {
+		trimmedURL = "https://" + trimmedURL
+	}
+
+	splits := strings.Split(trimmedURL, "/")
+	hardURL = strings.Join(splits[:3], "/")
+	softURL = "/" + strings.Join(splits[3:], "/")
+
+	defaultBranch := *repoBranch
+	// For switching to a branch & UnwrapPre will passively check if it exists
+	softURL += "/+/refs/heads/" + defaultBranch
+	return
 }
 
 func PrintInfo() {
@@ -181,10 +248,4 @@ func PrintInfo() {
 	fmt.Printf("CSV Path\t\t: %s\n", *csvPath)
 
 	fmt.Println("==================================================\n")
-}
-
-func must(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
